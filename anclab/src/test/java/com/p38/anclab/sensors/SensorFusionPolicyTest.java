2c1e982cbe94e063dec87f46ed97003a31406c25
Ossendjiver <39517879+Ossendjiver@users.noreply.github.com>
2026-09-08 13:01:47 +1000

    Test synchronous inputs and stable stereo output readiness

--- anclab/src/test/java/com/p38/anclab/sensors/SensorFusionPolicyTest.java
+++ anclab/src/test/java/com/p38/anclab/sensors/SensorFusionPolicyTest.java
@@ -8,19 +8,32 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 
 public final class SensorFusionPolicyTest {
-    @Test public void legacyRemainsPrimaryUntilBothStereoPathsAreComplete(){
+    @Test public void legacyRemainsPrimaryUntilBothSidedAcousticPathsAndStableOutputsAreComplete(){
         List<AncSensorDefinition> s=new ArrayList<>();
-        add(s,AncSensorDefinition.Type.ADXL345,AncSensorDefinition.Side.LEFT);
-        add(s,AncSensorDefinition.Type.ERROR_MIC,AncSensorDefinition.Side.LEFT);
-        add(s,AncSensorDefinition.Type.OUTPUT_CHANNEL,AncSensorDefinition.Side.LEFT);
+        addInput(s,AncSensorDefinition.Type.REFERENCE_MIC,AncSensorDefinition.Side.LEFT,"rp2040-main");
+        addInput(s,AncSensorDefinition.Type.ERROR_MIC,AncSensorDefinition.Side.LEFT,"rp2040-main");
+        addOutput(s,AncSensorDefinition.Side.LEFT,0.95);
         assertEquals(SensorFusionPolicy.ControllerPriority.LEGACY_PRIMARY,SensorFusionPolicy.priority(s));
-        add(s,AncSensorDefinition.Type.REFERENCE_MIC,AncSensorDefinition.Side.RIGHT);
-        add(s,AncSensorDefinition.Type.ERROR_MIC,AncSensorDefinition.Side.RIGHT);
-        add(s,AncSensorDefinition.Type.OUTPUT_CHANNEL,AncSensorDefinition.Side.RIGHT);
+        addInput(s,AncSensorDefinition.Type.REFERENCE_MIC,AncSensorDefinition.Side.RIGHT,"rp2040-main");
+        addInput(s,AncSensorDefinition.Type.ERROR_MIC,AncSensorDefinition.Side.RIGHT,"rp2040-main");
+        addOutput(s,AncSensorDefinition.Side.RIGHT,0.95);
         assertEquals(SensorFusionPolicy.ControllerPriority.SENSOR_PRIMARY,SensorFusionPolicy.priority(s));
     }
 
-    private static void add(List<AncSensorDefinition> list,AncSensorDefinition.Type type,AncSensorDefinition.Side side){
-        AncSensorDefinition s=new AncSensorDefinition();s.id=type+"-"+side;s.type=type;s.side=side;s.enabled=true;s.connected=true;s.calibrationState=AncSensorDefinition.CalibrationState.CALIBRATED;list.add(s);
+    @Test public void mismatchedInputClockOrJitteryOutputBlocksSensorPrimary(){
+        List<AncSensorDefinition> s=new ArrayList<>();
+        addInput(s,AncSensorDefinition.Type.REFERENCE_MIC,AncSensorDefinition.Side.LEFT,"rp2040-main");
+        addInput(s,AncSensorDefinition.Type.ERROR_MIC,AncSensorDefinition.Side.LEFT,"rp2040-main");
+        addInput(s,AncSensorDefinition.Type.REFERENCE_MIC,AncSensorDefinition.Side.RIGHT,"rp2040-other");
+        addInput(s,AncSensorDefinition.Type.ERROR_MIC,AncSensorDefinition.Side.RIGHT,"rp2040-main");
+        addOutput(s,AncSensorDefinition.Side.LEFT,0.95);addOutput(s,AncSensorDefinition.Side.RIGHT,0.40);
+        assertEquals(SensorFusionPolicy.ControllerPriority.LEGACY_PRIMARY,SensorFusionPolicy.priority(s));
+    }
+
+    private static void addInput(List<AncSensorDefinition> list,AncSensorDefinition.Type type,AncSensorDefinition.Side side,String clock){
+        AncSensorDefinition s=new AncSensorDefinition();s.id=type+"-"+side;s.type=type;s.side=side;s.clockDomain=clock;s.enabled=true;s.connected=true;s.calibrationState=AncSensorDefinition.CalibrationState.CALIBRATED;list.add(s);
+    }
+    private static void addOutput(List<AncSensorDefinition> list,AncSensorDefinition.Side side,double confidence){
+        AncSensorDefinition s=new AncSensorDefinition();s.id="out-"+side;s.type=AncSensorDefinition.Type.OUTPUT_CHANNEL;s.side=side;s.enabled=true;s.connected=true;s.outputRoute=AncSensorDefinition.OutputRoute.AUX;s.latencyConfidence=confidence;s.calibrationState=AncSensorDefinition.CalibrationState.CALIBRATED;list.add(s);
     }
 }
